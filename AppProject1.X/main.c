dd6edce12d6c6859d0504749403175039b8793ef
ToshiBiswas <92240916+ToshiBiswas@users.noreply.github.com>
2022-11-07 16:08:18 -0700

    StartedProject
    Created A main project

---
+++ AppProject1.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Tanmay
+ *
+ * Created on November 7, 2022, 4:07 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
