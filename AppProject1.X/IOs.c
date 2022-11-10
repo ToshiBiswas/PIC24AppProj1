3b280f62aca7ce2831527542dd86d47be6e3c18d
ToshiBiswas <92240916+ToshiBiswas@users.noreply.github.com>
2022-11-10 14:40:18 -0700

    Created previous files

--- AppProject1.X/IOs.c
+++ AppProject1.X/IOs.c
@@ -1,9 +1,10 @@
 #include "xc.h"
 #include "delay.h"
+#include "UART2.h"
 int button1 = 1;
 int button2 = 1;
 int button3 = 1;
-int interrupt = 1;
+int interupt = 1;
 
 int pb3flag = 0;
 int timerstart = 0;
@@ -12,6 +13,16 @@ int alarm = 0;
 int minutes = 0;
 int seconds = 0;
 
+void updatetime(){
+    //prints current minutes and seconds to the terminal
+    Disp2Dec(minutes);
+    Disp2String("m : ");
+    Disp2Dec(seconds);
+    Disp2String("s ");
+    if(alarm == 1){
+        Disp2String("-- ALARM");
+    }
+}
 void IOinit()
 {
     TRISAbits.TRISA2 = 1; //sets input PB1 to RA2
@@ -61,10 +72,10 @@ void IOcheck(){
             delay_ms(500); // while PB1 is pressed, update seconds every 0.5 seconds
             break;
         case(6): //PB3 pressed
-            interrupt = 0;
+            interupt = 0;
             pb3flag = 1;
             delay_ms(3000);
-            if(interrupt == 0){
+            if(interupt == 0){
                 // long press: stops the countdown and resets the timer to 00m 00s
                 pb3flag = 0;
                 timerstart = 0;
@@ -93,7 +104,7 @@ void IOcheck(){
 
                 if(minutes == 0 && seconds == 0){
                     timerstart = 0;
-                    alarm = 1
+                    alarm = 1;
                 }
 
                 updatetime();
@@ -124,17 +135,7 @@ void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void)
     button1 = PORTAbits.RA2;
     button2 = PORTBbits.RB4;
     button3 = PORTAbits.RA4;
-    interrupt = 1;
+    interupt = 1;
     return;
 }
 
-void updatetime(){
-    //prints current minutes and seconds to the terminal
-    Disp2Dec(minutes);
-    Disp2String("m : ");
-    Disp2Dec(seconds);
-    Disp2String("s ");
-    if(alarm == 1){
-        Disp2String("-- ALARM");
-    }
-}
